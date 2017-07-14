ffdf0def9039cab1a6ef6933cb1cbee46982e5bf
Tyler Phelan <tphelan@pivotal.io>
2017-07-14 11:57:28 -0400

    Add ping endpoint for testing.
    [#147784707]
    
    Signed-off-by: Joe Moore <jmoore@pivotal.io>

--- src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
@@ -28,6 +28,11 @@ public class InfoController {
         return new ApplicationInfo(springEnvironment.getActiveProfiles(), getServiceNames());
     }
 
+    @RequestMapping(value = "/ping")
+    public String ping() {
+        return "OK";
+    }
+
     @RequestMapping(value = "/service")
     public List<ServiceInfo> showServiceInfo() {
         if (cloud != null) {
